fcbbd913f1fde3673d7c90a3d0279ba4e345d458
Minh Nguyen <minhnh.91@gmail.com>
2015-09-14 10:57:55 +0200

    Initialise LegoRobot project

---
+++ src/Series2.java
@@ -0,0 +1,25 @@
+import ch.aplu.robotsim.LegoRobot;
+
+public class Series2 {
+
+	private LegoRobot robot;
+
+	Series2(int exerciseNum) {
+
+		this.robot = new LegoRobot();
+		switch (exerciseNum) {
+		case 1:
+			break;
+		}
+		robot.exit();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
