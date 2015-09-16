f95fd6fed91257c6151f0e04ffd7b81a3ddef9df
Bach Ha <bach.ha@smail.inf.h-brs.de>
2015-09-16 08:32:08 +0200

    Add comment to Ex5.1

--- src/Ex5/Ex5_1.java
+++ src/Ex5/Ex5_1.java
@@ -6,23 +6,27 @@ public class Ex5_1
 {
 	Ex5_1()
 	{
+		/*Instancing robot and part objects*/
 		NxtRobot robot = new NxtRobot();
 		Gear gear = new Gear();
 		LightSensor lSensor = new LightSensor(SensorPort.S1);
-		TouchSensor tSensor = new TouchSensor(SensorPort.S4);
+		TouchSensor tSensor = new TouchSensor(SensorPort.S4);	//S4: touch sensor at the rear
 		
+		/*Add parts to robot*/
 		robot.addPart(gear);
 		robot.addPart(lSensor);
 		robot.addPart(tSensor);
 		
-		gear.forward();
+		gear.forward();	//Start the robot
 		
 		while(true)
-		{
+		{	
+			/*If the touch sensor is pressed, the robot stops*/
 			if(tSensor.isPressed())
 			{
 				gear.stop();
 			}
+			/*If the light sensor is dark, the robot moves backward*/
 			else if(lSensor.getValue() < 500)
 			{
 				gear.backward();
@@ -36,6 +40,7 @@ public class Ex5_1
 		new Ex5_1();
 	}
 	
+	/*Simulation environment*/
 	static
 	{
 		NxtContext.showStatusBar(50);
