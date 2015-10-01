7dc19d8d310015565a2e9c2bd665630dafb1b059
Bach Ha <bach.ha@smail.inf.h-brs.de>
2015-10-01 12:43:28 +0200

    Add image message subcriber

--- ros/scripts/ball_detector_state_machine.py
+++ ros/scripts/ball_detector_state_machine.py
@@ -3,6 +3,7 @@
 #-*- encoding: utf-8 -*-
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
 import cv2
 import ball_detector_ros.ball_detector as bd
 import sys
@@ -12,71 +13,59 @@ Publisher class: Handle the publishing of output
 '''
 class publisher():
     def __init__(self):
-        self.pub = rospy.Publisher('event_out', String, queue_size=100, latch=False)
+        self.pub = rospy.Publisher('~position', String, queue_size=100, latch=False)
     def publish_output(self,msg):
-        self.pub.publish(msg)
+        self.pub.publish_position(msg)
 
-'''
-image_processor class: Image processing should be put inside this class
-'''
-class image_processor():
-    def __init__(self):
-	rospy.loginfo("Processor is instantiated.")
-
-    #Call this method 'run' to do the processing 
-    def run(self):	
-	bd.test_process_image(sys.argv[1])
-	output = "output here"	
-	return output
-	
-    def process_dummy_func(self):
-	img = cv2.imread('/home/bach/image.jpg',1)
-        cv2.imshow('image',img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-'''
-State machine, publisher and subcriber
-'''
-class run():
+class subcriber():
     state = "INIT"
+    input_img = Image()
     def __init__(self):
-	#Initiate node
-	rospy.init_node('open_cv_node', anonymous=False)
-        rospy.loginfo("Open_CV is now running")  
-	
-	#Create publisher, subcriber        
-	self.input_msg = rospy.Subscriber("event_in", String, self.input_msg_cb)
-	pub = publisher()
-
-	img_proc = image_processor()	#instantiate image_processor	
-
-	#Processing loop
-        r = rospy.Rate(10)
-        while not rospy.is_shutdown():
-	    if self.state == "PROC":	#If in processing state
-		output = String()
-		output.data = img_proc.run()	#Run the image processor
-                pub.publish_output(output)	#Publish output
-	    elif self.state == "INIT":	#If in initiate state
-		rospy.loginfo("Waiting for e_start") 
-    	    r.sleep()
+	self.event_in = rospy.Subscriber('~event_in', String, self.event_in_cb)
+	self.input_image = rospy.Subscriber('~input_image', Image, self.input_image_cb)
 
     #Callback function
-    def input_msg_cb(self, msg):
-        if msg.data == 'e_start':
+    def event_in_cb(self, msg):
+        if msg.data == 'e_start':	#Switch to start state
             rospy.loginfo("Starting")
 	    self.state = "PROC"                       
-        elif msg.data == 'e_stop':
+        elif msg.data == 'e_stop':	#Switch to stop state
             rospy.loginfo("Stoping")
 	    self.state = "INIT"
         else:
 	    rospy.loginfo("Waiting")
 
+    def input_image_cb(self, img):	#Get image message from sensor
+	self.input_img = img
+
+'''
+State machine, publisher and subcriber
+'''
+class run():
+    def __init__(self):
+	#Initiate node
+	rospy.init_node('ball_detector_node', anonymous=False)
+        rospy.loginfo("ball_detector_node is now running")  
 	
+	#Create publisher, subcriber        
+	pub = publisher()
+	sub = subcriber()
+
+	#Processing loop
+        r = rospy.Rate(10)
+        while not rospy.is_shutdown():
+	    if sub.state == "PROC" and sub.input_img.data:			#If in processing state and an image is received
+		position_output = String()
+		position_output.data = bd.test_process_image(sys.argv[1])	#Run the image processor. The output must be String
+                #pub.publish_position(position_output)				#Publish the String output
+	    elif sub.state == "INIT":						#If in initiate state
+		rospy.loginfo("Waiting for e_start") 	
+    	    r.sleep()
+
+'''	
 if __name__ == '__main__':    
     run()
-
+'''
 
     
 
