ec5d545b2899fc0ac2f72eb30e8aadf4d80b711b
Minh Nguyen <minhnh.91@gmail.com>
2015-10-03 11:18:25 +0200

    Initial Commit, Working Image Filter in main()

---
+++ ros/include/fc_youbot_hackathon/ball_detector.h
@@ -0,0 +1,10 @@
+#ifndef _BALL_DETECTOR_H_
+#define _BALL_DETECTOR_H_
+#include <ros/ros.h>
+
+#include <opencv/cv.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#endif //_BALL_DETECTOR_H_
+
