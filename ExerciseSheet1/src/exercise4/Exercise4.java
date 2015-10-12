23b50719ebdac45021e6a06667d31144f05a3cff
Minh Nguyen <minhnh.91@gmail.com>
2015-10-12 10:38:19 +0200

    Add Solution to Sheet 1, Exercise 4

--- ExerciseSheet1/src/exercise4/Exercise4.java
+++ ExerciseSheet1/src/exercise4/Exercise4.java
@@ -12,31 +12,71 @@ import java.util.Scanner;
 public class Exercise4 {
 
 	/**
+	 * Do approximation 10 decimal points at a time
+	 */
+	public double roundPi(int precision) {
+		double result = 0.0d;
+
+		int i = 0;
+		for (i = precision; i > 9; i -= 10) {
+			result += ((long) ((Math.PI - result) * Math.pow(10, precision - i)))
+					/ Math.pow(10, precision - i);
+		}
+
+		result += ((long) ((Math.PI - result) * Math.pow(10, precision)))
+				/ Math.pow(10, precision);
+
+		return result;
+	}
+
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Exercise4 ex4 = new Exercise4();
 		Scanner scanner = new Scanner(System.in);
-		float diameter = 0.0f;
+		double diameter = 0.0f;
 		int maxPrecision = 0;
 
-		System.out.print("Input the diameter of type float: ");
-		while (!scanner.hasNextFloat()) {
-			System.out.print("Float, please: ");
+		System.out.print("Input the diameter of type double: ");
+		while (!scanner.hasNextDouble()) {
+			System.out.print("Double, please: ");
 			scanner.nextLine();
 		}
-		diameter = scanner.nextFloat();
+		diameter = scanner.nextDouble();
 		scanner.nextLine();
 
-		System.out
-				.print("Input the maximum precision (Integer from 1 to 100): ");
-		while (!scanner.hasNextInt()) {
-			System.out.print("Integer between 1 and 100, please: ");
+		while (true) {
+			System.out
+					.print("Input the maximum precision (Integer from 0 to 20): ");
+			if (!scanner.hasNextInt()) {
+				System.out.print("Integer between 0 and 20, please: ");
+				scanner.nextLine();
+				continue;
+			}
+
+			maxPrecision = scanner.nextInt();
 			scanner.nextLine();
+
+			if (maxPrecision < 0 || maxPrecision > 20) {
+				System.out.println("Number out of acceptable range");
+				continue;
+			}
+
+			break;
 		}
-		maxPrecision = scanner.nextInt();
-		scanner.nextLine();
 
 		scanner.close();
+
+		double result = 0.0f;
+		for (int i = 0; i < maxPrecision + 1; i++) {
+			result = diameter * ex4.roundPi(i);
+			System.out.format("Circumference (precision = %3d): %10.20f\n", i,
+					result);
+		}
+
+		System.out.format("Circumference (no rounding): %10.20f", diameter
+				* Math.PI);
 
 	}
 
