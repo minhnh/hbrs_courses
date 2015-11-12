8f3a80ea44a7a8bf12eb92a468219bbff2474f51
Bach Ha <bach.ha@smail.inf.h-brs.de>
2015-11-12 18:22:44 +0100

    Create assignment06 folder, add tex files

---
+++ assignment06/src/greedy_search.cpp
@@ -0,0 +1,236 @@
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include "greedy_search.hpp"
+#include "state.hpp"
+#include <deque>
+
+using namespace std;
+
+Greedy_search::Greedy_search()
+{
+
+}
+
+void Greedy_search::run()
+{
+	size_x = 3;
+	size_y = 3;
+	int tiles[] = {1,  4,  8,
+				   3,  6,  2,
+				   0,  5,  7};
+
+	int index = 0;
+
+    cout << "Please choose heuristics: "<<endl;
+    cout << "(1) Manhattan distance"<<endl;
+    cout << "(2) Misplaced tiles"<<endl;
+    cin >> index;
+    if ( index == 1)
+    {
+		search(tiles,1);
+    } 
+    else if (index == 2) 
+    {
+		search(tiles,2);
+    } 
+    else 
+    {
+        cout << "Invalid heuristics index"<< endl;
+    }
+}
+
+void Greedy_search::search(int intput_map[], int heuristic_id)
+{
+	int expanded_node = 0;
+	State initial_state(intput_map,size_x,size_y);
+
+	deque<State> search_list;
+	deque<State> reached_state;
+	deque<State> solve_step;
+
+	search_list.push_front(initial_state);
+	initial_state.depth = 0;
+
+	while (search_list.size() > 0)
+	{
+		State current_state =  search_list.front();
+		reached_state.push_back(current_state);
+		search_list.pop_front();
+		if (current_state.h == 0)
+		{
+			solve_step.push_front(current_state);
+			break;
+		}
+		if(current_state.can_move_up)
+		{
+			expanded_node++;
+			insert_to_list(heuristic_id, current_state, UP, search_list, reached_state);
+		}
+
+		if(current_state.can_move_down)
+		{
+			expanded_node++;
+			insert_to_list(heuristic_id, current_state, DOWN, search_list, reached_state);
+		}
+
+		if(current_state.can_move_left)
+		{
+			expanded_node++;
+			insert_to_list(heuristic_id, current_state, LEFT, search_list, reached_state);
+		}
+
+		if(current_state.can_move_right)
+		{
+			expanded_node++;
+			insert_to_list(heuristic_id, current_state, RIGHT, search_list, reached_state);
+		}		
+	}
+
+	while(solve_step.front().depth > 0)
+	{
+		State last_state = move(solve_step.front().map,
+									revert_direction(solve_step.front().last_move));
+		for (int i = 0; i < reached_state.size(); i++)
+		{
+			if (compare_arrays(last_state.map,reached_state[i].map))
+			{
+				solve_step.push_front(reached_state[i]);
+				i == reached_state.size();
+			}
+		}
+	}
+
+	for (int i = 0; i < solve_step.size(); i++)
+	{
+		solve_step[i].print();
+	}
+
+	cout<<"Steps taken:"<<solve_step.size()<<endl;	
+	cout<<"Expanded nodes:"<<expanded_node<<endl;
+}
+
+void Greedy_search::insert_to_list(int heuristic_id, State &current_state, Direction d, std::deque<State> &search_list, std::deque<State> &reached_state)
+{
+	State next_state = move(current_state.map,d);
+
+	if (heuristic_id == 1)
+	{
+		current_state.h = current_state.h1;
+		next_state.h = next_state.h1;
+	}
+	else if (heuristic_id == 2)
+	{
+		current_state.h = current_state.h2;
+		next_state.h = next_state.h2;
+	}
+	next_state.depth = current_state.depth + 1; 
+	next_state.last_move = (int)d;
+	bool state_reached = false;
+	for (int i = 0; i < reached_state.size();i++)
+	{
+		if (compare_arrays(next_state.map,reached_state[i].map))
+		{
+			state_reached = true;
+			break;
+		}
+	}
+	if (!state_reached)
+	{
+		if (next_state.h < search_list.front().h or search_list.size() == 0)
+		{
+			search_list.push_front(next_state);
+		}
+		else if (next_state.h > search_list.back().h)
+		{
+			search_list.push_back(next_state);
+		}
+		else
+		{
+			search_list.push_front(next_state);
+			for (int i = 0; i+1 < search_list.size(); i++)
+			{
+				if (search_list[i].h > search_list[i+1].h)
+				{
+					State temp = search_list[i];
+					search_list[i] = search_list[i+1];
+					search_list[i+1] = temp;
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	}
+}
+
+State Greedy_search::move(int current_map[], Direction d)
+{
+	int map[size_x * size_y];
+	int zero_index = 0;
+	for (int i = 0; i < size_x * size_y; i++)
+	{
+		map[i] = current_map[i];
+		if (map[i] == 0)
+		{
+			zero_index = i;
+		}
+	}
+	if (d == UP)
+	{
+		map[zero_index] = map[zero_index - size_y];
+		map[zero_index - size_y] = 0;
+	}
+	else if (d == DOWN)
+	{
+		map[zero_index] = map[zero_index + size_y];
+		map[zero_index + size_y] = 0;
+	}
+	else if (d == LEFT)
+	{
+		map[zero_index] = map[zero_index - 1];
+		map[zero_index - 1] = 0;
+	}
+	else if (d == RIGHT)
+	{
+		map[zero_index] = map[zero_index + 1];
+		map[zero_index + 1] = 0;
+	}
+	State next_state(map, size_x, size_y);
+
+	return next_state;
+}
+
+bool Greedy_search::compare_arrays(int a[], int b[])
+{
+	for (int i = 0; i < sizeof(a); i++)
+	{
+		if (a[i] != b[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+Direction Greedy_search::revert_direction(int d)
+{
+	if (d == 0)
+	{
+		return DOWN;
+	}
+	else if (d == 1)
+	{
+		return UP;
+	}
+	else if (d == 2)
+	{
+		return RIGHT;
+	}
+	else if (d == 3)
+	{
+		return LEFT;
+	}
+	return UP;
+}
---
+++ assignment06/src/state.cpp
@@ -0,0 +1,145 @@
+#include <fstream>
+#include <iostream>
+#include <stdlib.h> 
+#include "state.hpp"
+
+using namespace std;
+
+State::State(int Tiles[], int size_x, int size_y)
+{
+	for (int i = 0; i < size_x * size_y; i++)
+	{
+		map[i] = Tiles[i];
+		if (map[i] == 0)
+		{
+			zero_index = i;
+		}
+	}
+	map_size_x = size_x;
+	map_size_y = size_y;
+	
+	h = 1;
+	h1 = find_heuristics_1();
+	h2 = find_heuristics_2();
+	depth = 0;
+	
+	check_expandability();
+}
+
+void State::check_expandability()
+{
+	if (zero_index > map_size_y)
+	{
+		can_move_up = true;
+	}
+	else
+	{
+		can_move_up = false;
+	}
+	
+	if (zero_index < map_size_x * (map_size_y -1))
+	{
+		can_move_down = true;
+	}
+	else
+	{
+		can_move_down = false;
+	}
+	
+	if (zero_index % map_size_x != 0)
+	{
+		can_move_left = true;
+	}
+	else
+	{
+		can_move_left = false;
+	}
+	
+	if (zero_index % map_size_x != (map_size_x -1))
+	{
+		can_move_right = true;
+	}
+	else
+	{
+		can_move_right = false;
+	}
+}
+
+
+//Manhattan distance
+int State::find_heuristics_1()
+{
+	int heuristics = 0;
+	for (int y = 0; y < map_size_y; y++)
+	{
+		for (int x = 0; x < map_size_x; x++)
+		{
+			if (get_value_at(x,y) != 0)
+			{
+				int correct_x = (((map_size_x * map_size_y -1) - get_value_at(x,y)) 
+										% map_size_y);
+				int correct_y = (((map_size_x * map_size_y -1) - get_value_at(x,y)) 
+										/ map_size_y);
+				heuristics = heuristics 
+								+ abs(correct_x - x) 
+								+ abs(correct_y - y);
+			}
+		}
+	}
+	return heuristics;
+}
+
+//Number of displaced tiles
+int State::find_heuristics_2()
+{
+	int heuristics = 0;
+	for (int y = 0; y < map_size_y; y++)
+	{
+		for (int x = 0; x < map_size_x; x++)
+		{
+			int correct_value = (map_size_x * map_size_y -1) 
+								- (x + map_size_y * y);			
+			if (get_value_at(x,y) != correct_value 
+					&& get_value_at(x,y) != 0 )
+			{
+				heuristics++;
+			}
+		}
+	}
+	return heuristics;
+}
+
+void State::print()
+{
+	cout<<endl;
+	for (int y = 0; y < map_size_y; y++)
+	{
+		for (int x = 0; x < map_size_x; x++)
+		{
+			int value = get_value_at(x,y);
+			if (value == 0)
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << value;		
+			}
+			if (x < 2)
+			{
+				cout << "|";
+			}
+		}
+		cout << endl;
+	}
+}
+
+int State::get_value_at(int x, int y)
+{
+	return map[x + y * map_size_y];
+}
+
+int State::get_index(int x, int y)
+{
+	return x + y * map_size_y;
+}
