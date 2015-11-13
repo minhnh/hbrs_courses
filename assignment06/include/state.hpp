80db0a675cb5d811666cb0057c2953113fff55c7
Minh Nguyen <minhnh.91@gmail.com>
2015-11-13 11:12:07 +0100

    copy files from assignment 5

---
+++ assignment06/include/state.hpp
@@ -0,0 +1,35 @@
+#ifndef STATE_HPP
+#define STATE_HPP
+class State
+{
+    public:
+        State(int Tiles[], int size_x, int size_y);
+        ~State();
+        int map[9];
+        int map_size_x;
+        int map_size_y;
+
+        int h;
+        int h1;
+        int h2;
+        int zero_index;
+        int depth;
+        bool can_move_up;
+        bool can_move_down;
+        bool can_move_left;
+        bool can_move_right;
+        int last_move;
+
+        void check_expandability();
+        //Manhattan distance
+        int find_heuristics_1();
+        //Misplaced tiles
+        int find_heuristics_2();
+
+        int get_value_at(int x, int y);
+        int get_index(int x, int y);
+
+        void print();
+    private:
+};
+#endif
---
+++ assignment06/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include "greedy_search.hpp"
+using namespace std;
+
+int main(int arc, char* argv[])
+{
+    Greedy_search solver;
+    solver.run();
+    return 1;
+}
