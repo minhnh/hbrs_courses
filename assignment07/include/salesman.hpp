a1accf6bfba054481273f278cbd52dc90529fa66
Minh Nguyen <minhnh.91@gmail.com>
2015-11-22 18:27:07 +0100

    move private functions to private

--- assignment07/include/salesman.hpp
+++ assignment07/include/salesman.hpp
@@ -10,15 +10,17 @@ using namespace std;
 class Salesman {
     private:
         vector<City> cities;
-    public:
-        Salesman(ifstream & in_file);
-        ~Salesman();
+
         vector<City> hillClimb(vector<City> cities_in);
-        void random_restart_hill_climb();
         float fullDist(vector<City> cities);
         float distance(City city1, City city2);
         vector<City> swap(vector<City> cities, int i, int j);
         vector<City> readFile(ifstream & in_file);
+
+    public:
+        Salesman(ifstream & in_file);
+        ~Salesman();
+        void random_restart_hill_climb();
         void print_cities(vector<City>);
 };
 
