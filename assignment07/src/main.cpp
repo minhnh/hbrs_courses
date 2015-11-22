c44e437c1869c083c8df68993d0ad84e6e939557
Minh Nguyen <minhnh.91@gmail.com>
2015-11-22 04:20:44 +0100

    Add correct logic to random restart

--- assignment07/src/main.cpp
+++ assignment07/src/main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char* argv[]) {
     const char* file_name;
     Salesman salesman = Salesman();
 
+    // Extract file name from argument if exist, otherwise use default file
+    // name
     if (argc == 1) {
         file_name = CITIES_FILE;
     }
@@ -18,6 +20,7 @@ int main(int argc, char* argv[]) {
         file_name = argv[1];
     }
 
+    // Try to open file
     ifstream in_file(file_name);
     if (in_file)
     {
@@ -31,21 +34,34 @@ int main(int argc, char* argv[]) {
 
     clock_t startTime = clock();
     srand(unsigned(time(0)));
-    random_shuffle(cities.begin(), cities.end());
-    cities = salesman.swap(cities, 0, 1);
-    cout << "Initial list of cities" << endl;
-    for (vector<int>::size_type i = 0; i != cities.size(); i++) {
-        cout << cities[i].getName() << " " << cities[i].getXCoord() << " "
-            << cities[i].getYCoord() << endl;
-    }
-    cout << endl << "Distance before " << salesman.fullDist(cities) << endl;
-    cities = salesman.hillClimb(cities);
-    cout << endl << "Optimal list of cities" << endl;
-    for (vector<int>::size_type i = 0; i != cities.size(); i++) {
-        cout << cities[i].getName() << " " << cities[i].getXCoord() << " "
+    float cur_full_dist = salesman.fullDist(cities);
+    float nxt_full_dist = 0.0f;
+    while (true) {
+        random_shuffle(cities.begin(), cities.end());
+        cout << "Initial list of cities" << endl;
+        for (vector<int>::size_type i = 0; i != cities.size(); i++) {
+            cout << cities[i].getName() << " " << cities[i].getXCoord() << " "
                 << cities[i].getYCoord() << endl;
+        }
+        cout << endl << "Distance before " << salesman.fullDist(cities) << endl;
+        cities = salesman.hillClimb(cities);
+        nxt_full_dist = salesman.fullDist(cities);
+        cout << endl << "List after hillClimb" << endl;
+        for (vector<int>::size_type i = 0; i != cities.size(); i++) {
+            cout << cities[i].getName() << " " << cities[i].getXCoord() << " "
+                    << cities[i].getYCoord() << endl;
+        }
+        cout << endl << "Distance after " << nxt_full_dist << endl << endl;
+        if (nxt_full_dist >= cur_full_dist)
+        {
+            cout << "Reached local maximum of random restart." << endl;
+            break;
+        }
+        else
+        {
+            cur_full_dist = nxt_full_dist;
+        }
     }
-    cout << endl << "Distance after " << salesman.fullDist(cities) << endl;
     // some code here
     // to compute its execution duration in runtime
     cout << double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." << endl;
