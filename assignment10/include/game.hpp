a03c3d1a59711e7d7c8c4650fa31597503bd807a
Minh Nguyen <minhnh.91@gmail.com>
2015-12-13 22:48:28 +0100

    fix indentation and add function stubs

--- assignment10/include/game.hpp
+++ assignment10/include/game.hpp
@@ -2,26 +2,27 @@
 #define _ASSIGNMENT10_GAME_H_
 #include <vector>
 
-class Game 
+class Game
 {
     private:
-		int size_x;
-		int size_y;
-		bool is_finished;
-		bool player1;
-		bool player2;
-		std::vector<int> current_map;
-		void decide_move(bool Player, int symbol, std::vector<int> &map);
-		void human_move(int symbol, std::vector<int> &map);
-		void computer_move(int symbol, std::vector<int> &map);
-		
+        int size_x;
+        int size_y;
+        bool is_finished;
+        bool player1;
+        bool player2;
+        std::vector<int> current_map;
+        void decide_move(bool Player, int symbol, std::vector<int> &map);
+        void human_move(int symbol, std::vector<int> &map);
+        void computer_move(int symbol, std::vector<int> &map);
+        void decision_alpha_beta();
+        void decision_minimax();
 
     public:
-		Game(int x, int y, int number_of_human_player);
-		~Game();
-		int set_value_at(int x, int y, int symbol, std::vector<int> &map);
-		int get_value_at(int x, int y, std::vector<int> map);
-		void print_map(std::vector<int> map);
+        Game(int x, int y, int number_of_human_player);
+        ~Game();
+        int set_value_at(int x, int y, int symbol, std::vector<int> &map);
+        int get_value_at(int x, int y, std::vector<int> map);
+        void print_map(std::vector<int> map);
 
 
 };
--- assignment10/include/state.hpp
+++ assignment10/include/state.hpp
@@ -2,31 +2,31 @@
 #define _ASSIGNMENT10_STATE_H_
 #include <vector>
 
-#define EMPTY	0
+#define EMPTY   0
 #define X       1
-#define O		-1
-#define EDGE 	2
+#define O       -1
+#define EDGE    2
 
-class State 
+class State
 {
     private:
-		int size_x;
-		int size_y;
-		std::vector<int> map;
-		int ultility;
-		int minimax_value;
-		
-		int get_value_at(int x, int y);
-		
+        int size_x;
+        int size_y;
+        std::vector<int> map;
+        int ultility;
+        int minimax_value;
+
+        int get_value_at(int x, int y);
+
     public:
-		State(std::vector<int>, int x, int y);
-		~State();
-		void calculate_ultility();
-		int get_ultility();
-		int get_minimax();
-		void set_minimax(int value);
-		std::vector<int> get_map();
-		void print_map();
+        State(std::vector<int>, int x, int y);
+        ~State();
+        void calculate_ultility();
+        int get_ultility();
+        int get_minimax();
+        void set_minimax(int value);
+        std::vector<int> get_map();
+        void print_map();
 };
 
 #endif /* _ASSIGNMENT10_STATE_H_ */
