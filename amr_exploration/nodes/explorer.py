f39f06926656696a892380b187146a1793ca0956
Minh Nguyen <minhnh.91@gmail.com>
2016-01-15 17:57:15 +0100

    add basic exploration

--- amr_exploration/nodes/explorer.py
+++ amr_exploration/nodes/explorer.py
@@ -14,10 +14,10 @@ from nav_msgs.msg import MapMetaData, OccupancyGrid
 from amr_srvs.srv import PlanPathRequest, PlanPath, PlanPathResponse
 
 class ExplorerNode:
-    
-    
+
+
     def __init__(self):
-        
+
         rospy.init_node(NODE)
         """
             Publishers
@@ -32,18 +32,20 @@ class ExplorerNode:
                                                      Frontiers,
                                                      self._frontier_callback,
                                                      queue_size=1)
-        
+
         self._path_plan_client = rospy.ServiceProxy('path_planner/plan_path',
                                                     PlanPath)
         self._execute_path_client = actionlib.SimpleActionClient('path_executor/execute_path',
                                                                  ExecutePathAction)
+        self._execute_path_client.wait_for_server()
         self._tf = tf.TransformListener()
         self._last_map_publication = rospy.Time.now()
         rospy.loginfo('Started [explorer] node.')
         self._targets = None
+        self._sizes = None
         self._world_is_explored = False
-        
-    
+
+
     def explore(self):
         while not rospy.is_shutdown() and not self._world_is_explored:
             if self._targets is None:
@@ -52,25 +54,57 @@ class ExplorerNode:
                 continue
             """
             Your code here
-            
+
             Centroids of the frontier clusters are stored in self._targets
-            
+
             Check Frontiers essage for more info.
-            
+
             * Choose a target
             * Plan a path to it
             * Execute the path
             .. etc
             """
-            pass
+            rospy.loginfo("targets: %d, sizes: %d", len(self._targets), len(self._sizes))
+            if len(self._sizes) < 1:
+                continue
+
+            # Setup service request to path_plan
+            path_plan_req = PlanPathRequest()
+            # Choose largest cluster
+            target_index = min(xrange(len(self._sizes)), key=self._sizes.__getitem__)
+            target = self._targets[target_index]
+            # start at current pose
+            start = self._get_current_pose()
+            # Check None poses
+            if start is None:
+                rospy.logerr(self.__class__.__name__ + ": _get_current_pose() return None")
+                continue
+            elif target is None:
+                rospy.logerr(self.__class__.__name__ + ": None target")
+                continue
+            else:
+                path_plan_req.start = start
+                path_plan_req.end = target
+
+            # Send request
+            try:
+                path_plan_resp = self._path_plan_client(path_plan_req)
+                path_goal = ExecutePathGoal()
+                path_goal.path = path_plan_resp.path
+                path_goal.skip_unreachable = True
+                self._execute_path_client.send_goal(path_goal)
+            except rospy.ServiceException as e:
+                rospy.logerr(self.__class__.__name__ + ': Path plan service failed: ' + str(e))
+
         rospy.loginfo('World is explored, exiting.')
-        
-    
+
+
     def _get_current_pose(self):
         try:
+            time = self._tf.getLatestCommonTime("odom", "base_link")
             position, quaternion = self._tf.lookupTransform("odom",
                                                             "base_link",
-                                                            rospy.Time.now())
+                                                            time)
         except Exception as ex:
             rospy.logwarn('Unable to lookup the base transform '
                           'Reason: {}.'.format(ex.message))
@@ -79,11 +113,12 @@ class ExplorerNode:
         pose.x, pose.y = position[0], position[1]
         pose.theta = tf.transformations.euler_from_quaternion(quaternion)[2]
         return pose
-    
-    
+
+
     def _frontier_callback(self, frontiers_msg):
         rospy.loginfo("EXPLORER: FRONTIER CALLBACK")
         self._targets = frontiers_msg.centroids
+        self._sizes = frontiers_msg.sizes
         pass
 
 
