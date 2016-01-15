6d1693bd57026c4fae684a7219156a33ec8363c9
Minh Nguyen <minhnh.91@gmail.com>
2016-01-15 22:59:08 +0100

    add logic to move forward when no frontier is found

--- amr_exploration/nodes/explorer.py
+++ amr_exploration/nodes/explorer.py
@@ -14,7 +14,7 @@ from nav_msgs.msg import MapMetaData, OccupancyGrid
 from amr_srvs.srv import PlanPathRequest, PlanPath, PlanPathResponse
 
 class ExplorerNode:
-
+    _NO_TARGET_THRESHOLD = 5
 
     def __init__(self):
 
@@ -44,12 +44,13 @@ class ExplorerNode:
         self._targets = None
         self._sizes = None
         self._world_is_explored = False
+        self._no_target_counter = 0
 
 
     def explore(self):
         while not rospy.is_shutdown() and not self._world_is_explored:
             if self._targets is None:
-                rospy.logwarn('No frontier information acquired, waiting')
+                rospy.logwarn(self.__class__.__name__ + ': No frontier info, waiting')
                 rospy.sleep(2.0)
                 continue
             """
@@ -64,39 +65,58 @@ class ExplorerNode:
             * Execute the path
             .. etc
             """
-            rospy.loginfo("targets: %d, sizes: %d", len(self._targets), len(self._sizes))
             if len(self._sizes) < 1:
+                rospy.logwarn(self.__class__.__name__ + ': No frontier info, going forward')
+                # Move forward
+                current_pose = None
+                while current_pose is None:
+                    current_pose = self._get_current_pose()
+                current_pose.x = current_pose.x + 1.0
+                self._targets = [current_pose]
+                self._sizes = [0.0]
                 continue
 
-            # Setup service request to path_plan
-            path_plan_req = PlanPathRequest()
-            # Choose largest cluster
-            target_index = min(xrange(len(self._sizes)), key=self._sizes.__getitem__)
-            target = self._targets[target_index]
-            # start at current pose
-            start = self._get_current_pose()
-            # Check None poses
-            if start is None:
-                rospy.logerr(self.__class__.__name__ + ": _get_current_pose() return None")
-                continue
-            elif target is None:
-                rospy.logerr(self.__class__.__name__ + ": None target")
-                continue
-            else:
-                path_plan_req.start = start
-                path_plan_req.end = target
+            rospy.loginfo("targets: %d, sizes: %d", len(self._targets), len(self._sizes))
 
-            # Send request
-            try:
-                path_plan_resp = self._path_plan_client(path_plan_req)
-                path_goal = ExecutePathGoal()
-                path_goal.path = path_plan_resp.path
-                path_goal.skip_unreachable = True
-                self._execute_path_client.send_goal(path_goal)
-            except rospy.ServiceException as e:
-                rospy.logerr(self.__class__.__name__ + ': Path plan service failed: ' + str(e))
+            #?
+            if self._no_target_counter > self._NO_TARGET_THRESHOLD:
+                self._world_is_explored = True
+            #?
 
-        rospy.loginfo('World is explored, exiting.')
+            while len(self._targets) > 0:
+                # Setup service request to path_plan
+                path_plan_req = PlanPathRequest()
+                # Choose largest cluster
+                target_index = min(xrange(len(self._sizes)), key=self._sizes.__getitem__)
+                target = self._targets[target_index]
+                # start at current pose
+                start = self._get_current_pose()
+                # Check None poses
+                if start is None:
+                    rospy.logerr(self.__class__.__name__ + ": _get_current_pose() return None")
+                    continue
+                elif target is None:
+                    rospy.logerr(self.__class__.__name__ + ": None target")
+                    continue
+                else:
+                    path_plan_req.start = start
+                    path_plan_req.end = target
+
+                # Send request
+                try:
+                    path_plan_resp = self._path_plan_client(path_plan_req)
+                    path_goal = ExecutePathGoal()
+                    path_goal.path = path_plan_resp.path
+                    path_goal.skip_unreachable = True
+                    self._execute_path_client.send_goal(path_goal)
+                except rospy.ServiceException as e:
+                    rospy.logerr(self.__class__.__name__ + ': Path plan service failed: ' + str(e))
+
+                # Remove arrived target
+                self._targets.remove(self._targets[target_index])
+                self._sizes.remove(self._sizes[target_index])
+
+        rospy.loginfo(self.__class__.__name__ + ': World is explored, exiting.')
 
 
     def _get_current_pose(self):
@@ -106,8 +126,8 @@ class ExplorerNode:
                                                             "base_link",
                                                             time)
         except Exception as ex:
-            rospy.logwarn('Unable to lookup the base transform '
-                          'Reason: {}.'.format(ex.message))
+            rospy.logwarn(self.__class__.__name__ + ': Unable to lookup base transform '
+                          'Reason: ' + str(ex))
             return None
         pose = Pose2D()
         pose.x, pose.y = position[0], position[1]
@@ -116,9 +136,14 @@ class ExplorerNode:
 
 
     def _frontier_callback(self, frontiers_msg):
-        rospy.loginfo("EXPLORER: FRONTIER CALLBACK")
+        rospy.loginfo(self.__class__.__name__ + ": FRONTIER CALLBACK")
         self._targets = frontiers_msg.centroids
         self._sizes = frontiers_msg.sizes
+        # Increment counter if frontier unable to find any centroids, else reset
+        if len(self._targets) == 0:
+            self._no_target_counter = self._no_target_counter + 1
+        else:
+            self._no_target_counter = 0
         pass
 
 
