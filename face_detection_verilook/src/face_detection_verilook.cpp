c83381ebec8a071b774ed2bf2da521f62e375c93
Minh Nguyen <minh.nguyen@smail.inf.h-brs.de>
2016-04-15 18:09:07 +0200

    add dependency for roscpp, some minor node code

--- face_detection_verilook/src/face_detection_verilook.cpp
+++ face_detection_verilook/src/face_detection_verilook.cpp
@@ -1,4 +1,11 @@
+#include <ros/ros.h>
 
-int main() {
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    ros::init(argc, argv, "face_detection_verilook");
+    ros::NodeHandle nh;
+    ROS_INFO("face_detection_verilook: beginning main");
+    ros::spin();
     return 0;
 }
