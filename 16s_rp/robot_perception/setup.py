6cec7640474557c108ef5fd32cd41afc3a72b748
Minh Nguyen <minhnh.91@gmail.com>
2016-04-26 18:23:39 +0200

    update folder structure for robot_perception

--- 16s_rp/robot_perception/setup.py
+++ 16s_rp/robot_perception/setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(
-    name='16s_robot_perception',
+    name='robot_perception',
     version='1.0',
     description='Solution library for 16SS Robot Perception class',
     author='Minh Nguyen',
     author_email='minh.nguyen@smail.inf.h-brs.de',
     packages=['normalized_dlt', 'ransac'],
-    package_dir={'' : 'src'}
+    package_dir={'' : 'src'},
 )
