f021bd3a1f9fb2fdc50fcf2209a0d8515f354656
Minh Nguyen <minhnh.91@gmail.com>
2016-04-28 23:02:08 +0200

    remove preceding whitespaces

--- verilook_ros/face_detection_verilook/include/face_detection_verilook/face_detection_verilook_node.h
+++ verilook_ros/face_detection_verilook/include/face_detection_verilook/face_detection_verilook_node.h
@@ -3,12 +3,13 @@
 
 #include <ros/ros.h>
 
-    class FaceDetectionVerilookNode
-    {
-    public:
-        FaceDetectionVerilookNode(ros::NodeHandle nh);
-        ~FaceDetectionVerilookNode(void);
-    protected:
-        ros::NodeHandle node_handle_;
-    };
+class FaceDetectionVerilookNode
+{
+public:
+    FaceDetectionVerilookNode(ros::NodeHandle nh);
+    ~FaceDetectionVerilookNode(void);
+protected:
+    ros::NodeHandle node_handle_;
+};
+
 #endif // FACE_DETECTION_VERILOOK_NODE_HPP_INCLUDED
