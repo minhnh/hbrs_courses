f2022e79a1c56abe9b9c444069895d4fc7805fed
Minh Nguyen <minhnh.91@gmail.com>
2016-05-02 10:23:33 +0200

    add adaptive number of iterations calculation

--- 16s_rp/robot_perception/src/homography_estimation/ransac.py
+++ 16s_rp/robot_perception/src/homography_estimation/ransac.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+
 import random
 import numpy as np
 from homography_estimation import normalized_dlt, util
 
 
 class RANSAC:
-    def __init__(self, error_threshold, p_outlier, p=0.99, sample_size_min=4):
+    """
+    RANSAC for DLT algorithm, referenced from Hartley & Zisserman's book
+    Multiple View Geometry (chapter 4)
+    """
+    def __init__(self, error_threshold, p_outlier, p=0.99, sample_size_min=4, max_iteration_num=100):
         """
         Init function for RANSAC
 
@@ -17,6 +23,7 @@ class RANSAC:
         self._p_outlier = p_outlier
         self._p = p
         self._sample_size_min = sample_size_min
+        self._max_iteration_num = max_iteration_num
 
     def get_homography(self, x, x_tick):
         """
@@ -32,7 +39,7 @@ class RANSAC:
             print("ransac: Need at least 4 samples for homography calculation.")
             return None
 
-        iteration_num = self._calculate_iteration_num()
+        iteration_num = self._calculate_iteration_num_adaptive(x, x_tick)
         sample_num = len(x)
         inlier_num_threshold = int((1 - self._p_outlier) * sample_num * 0.8)
 
@@ -57,11 +64,35 @@ class RANSAC:
                 inlier_indices.append(j)
         return inlier_indices
 
-    def _calculate_iteration_num(self):
+    def _calculate_iteration_num_adaptive(self, x, x_tick):
         """
         N = log10(1-p)/log10(1 - (1 - p_outlier)^sample_size_min)
         """
-        iteration_num = np.log10(1 - self._p) / np.log10(1 - np.power(1 - self._p_outlier, self._sample_size_min))
+        iteration_num = self._max_iteration_num
+        sample_count = 0
+        total_sample_num = len(x)
+
+        while iteration_num > sample_count:
+            random_indices = self._get_random_indices(total_sample_num)
+            homography = normalized_dlt.get_homography(x[random_indices], x_tick[random_indices])
+            inlier_indices = self._get_inlier_indices(x, x_tick, homography, total_sample_num)
+            p_outlier = 1. - len(inlier_indices)/total_sample_num
+            iteration_num = self._calculate_iteration_num(p_outlier=p_outlier)
+            sample_count = sample_count + 1
+
+        return int(iteration_num)
+
+    def _calculate_iteration_num(self, p_outlier=None):
+        """
+        N = log10(1-p)/log10(1 - (1 - p_outlier)^sample_size_min)
+
+        :param p_outlier: custom outlier ratio for adaptive _calculate_iteration_num_adaptive
+        """
+        if p_outlier is None:
+            p_outlier = self._p_outlier
+        if np.isclose(p_outlier, 1.0):
+            return self._max_iteration_num
+        iteration_num = np.log10(1 - self._p) / np.log10(1 - np.power(1 - p_outlier, self._sample_size_min))
 
         return int(iteration_num)
 
