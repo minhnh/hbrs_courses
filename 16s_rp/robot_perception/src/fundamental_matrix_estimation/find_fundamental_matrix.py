9d05f01ca8e1888f1a2d40d17973e11aab4f55bf
Minh Nguyen <minhnh.91@gmail.com>
2016-05-30 23:57:26 +0200

    add skeleton similar to normalized dlt

--- 16s_rp/robot_perception/src/fundamental_matrix_estimation/find_fundamental_matrix.py
+++ 16s_rp/robot_perception/src/fundamental_matrix_estimation/find_fundamental_matrix.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from homography_estimation import util
+
+
+def perform_normalized_eight_points(x, x_tick):
+    """
+    :param x:
+    :param x_tick:
+    """
+    # Normalize x and x_tick
+    (x_norm_matrix, x_norm) = util.normalize(x)
+    (x_tick_norm_matrix, x_tick_norm) = util.normalize(x_tick)
+    # DLT algorithm
+    f_norm = perform_eight_points(x_norm, x_tick_norm)
+    # Denormalize H_norm
+    fundamental_matrix = np.dot(np.dot(np.linalg.inv(x_norm_matrix), f_norm), x_norm_matrix)
+    return fundamental_matrix
+
+
+def perform_eight_points(x, x_tick):
+    pass
+
+
+def construct_A_i_matrix():
+    pass
