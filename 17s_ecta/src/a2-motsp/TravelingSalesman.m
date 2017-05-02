743ba8f710ed34ee9e350240485de1eab93cdf30
Minh Nguyen <minhnh.91@gmail.com>
2017-05-02 10:39:21 +0200

    add permutation encoding class

--- 17s_ecta/src/a2-motsp/TravelingSalesman.m
+++ 17s_ecta/src/a2-motsp/TravelingSalesman.m
@@ -5,29 +5,34 @@
 POPULATION_SIZE = 20;
 cities = importdata('cities_small.csv');
 coords = cities.data(:, 2:3);
+numCities = size(coords, 1);
 
 %% Precompute euclidean distance between cities
 distances = PrecomputeDistance(coords);
 
 %% Initialize population
-population = GeneratePopulation(coords, POPULATION_SIZE);
-fitness = GetFitness(population, distances);
+ie = GeneticEncoding.PermutationEncoding(POPULATION_SIZE, numCities, 0, distances,...
+                                         @GeneratePopulation, @GetFitness,...
+                                         @SelectWinners, @SingleCrossover,...
+                                         @Mutate, @CheckConvergence, true);
 
-VisualizeCities(coords, population(1, :))
+VisualizeCities(coords, ie.Population(1, :))
 
 %% Helper Functions for TSP
 function distances = PrecomputeDistance(coords)
     distances = squareform(pdist(coords, 'euclidean'));
 end
 
-function population = GeneratePopulation(coords, numGenome)
-    population = zeros(numGenome, size(coords, 1));
-    for i = 1:numGenome
-        population(i, :) = randperm(size(coords, 1));
+function GeneratePopulation(obj, populationSize, numGene, ~)
+    population = zeros(populationSize, numGene);
+    for i = 1:populationSize
+        population(i, :) = randperm(numGene);
     end
+    disp(numGene)
+    set(obj, 'Population', population);
 end
 
-function fitness = GetFitness(genomes, distances)
+function fitness = GetFitness(genomes, ~, distances)
     numGenes = size(genomes, 2);
     numGenomes = size(genomes, 1);
     fitness = zeros(numGenomes, 1);
@@ -37,8 +42,31 @@ function fitness = GetFitness(genomes, distances)
             genePair = num2cell(genomes(i, j:j + 1));
             distanceSum = distanceSum + distances(genePair{:});
         end
-        fitness(i) = distanceSum;
+        fitness(i) = -distanceSum;
     end
+end
+
+function winners = SelectWinners(obj, selection_size)
+    winners = zeros(selection_size, obj.BitLength);
+    for i = 1:selection_size
+        parentIndices = randperm(size(obj.Population, 1), 2);
+        parents = obj.Population(parentIndices, :);
+        fitness = obj.funcGetFitness(parents, obj.Target, obj.Constraints);
+        [~, maxArg] = max(fitness);
+        winners(i, :) = parents(maxArg, :);
+    end
+end
+
+function Mutate()
+end
+
+function child = SingleCrossover(obj, parents)
+    midPoint = int16(obj.BitLength / 2 + 0.5);
+    child = [parents(1, 1 : midPoint), parents(2, midPoint + 1 : obj.BitLength)];
+end
+
+function converging = CheckConvergence(~)
+    converging = false;
 end
 
 %% Visualization of cities
