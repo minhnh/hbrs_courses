c1038a7f916463f29e862d0536f25a6670eb3867
Minh Nguyen <minhnh.91@gmail.com>
2017-06-02 10:25:05 +0200

    migrate src to a separate repo

--- 17s_ecta/src/t3-function_minimize/GetFitness.m
+++
@@ -1,5 +0,0 @@
-function fitness = GetFitness(genomes, targetFunction, ~)
-%GETFITNESS Fitness function for function minimization problem
-    fitness = -targetFunction(genomes');
-end
-
--- 17s_ecta/src/t3-function_minimize/frastrigin.m
+++
@@ -1,7 +0,0 @@
-%% Rastrigin function
-function f=frastrigin(x)
-  N = size(x,1);
-  f = (N/20)*807.06580387678 - (10 * (N-sum(cos(2*pi*x),1)) + sum(x.^2,1));
-  f(any(abs(x) > 5.12)) = 1e2*N;
-  f = -f;
-end
